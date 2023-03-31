5ed2a07be662859793bc0553da0d649e17914183
SmrutiSucharitaMishra <smrutim89@gmail.com>
2023-03-31 22:36:23 +0530

    day 10

---
+++ DAY-10/SLEEP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	while(a--){
+	    int x;
+	    cin>>x;
+	    if(x<7)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
---
+++ DAY-10/fIRST UNIQUE character in a sttring.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26]={0};
+        int ans = -1;
+        for(int i=0; i<s.length(); i++){
+            int index = s[i]-'a';
+            arr[index]++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(arr[s[i]-'a'] == 1) return i; 
+        }
+        return ans;
+    }
+};
